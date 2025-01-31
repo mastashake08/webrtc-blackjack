15333c65e18b2b50c9f6646e81036965943693a3
Jyrone Parker <jyrone.parker@gmail.com>
2025-01-30 22:57:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/pwa-icon-192.png",
+            src: "/webrtc-blackjack/pwa-icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-icon-512.png",
+            src: "/webrtc-blackjack/pwa-icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
